459c39f8c027fd72860589708d88d8f6d7d116e2
Archiektor <archiektor@gmail.com>
2019-10-31 17:08:37 +0100

    - added logic to 3 condition - added mobile responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,17 @@
-/*
-*
+/* Check 3 variables
+    “Name”: field.length > 1
+    “Email”: pole musi być poprawnym adresem e-mail
+    “Subject”: pole nie może zawierać więcej niż 60 znaków
+
+    reset form
 * */
 
 
-let name, email, subject, text, correctEmail, correctSubject;
+let name, email, subject, text, arr, formPath;
+
+formPath = document.getElementById("small_form");
+
+arr = [];
 
 function validate() {
     name = document.getElementById("name").value;
@@ -11,16 +19,23 @@ function validate() {
     subject = document.getElementById("subject").value;
     text = document.getElementById("comments").value;
 
-    correctEmail = emailIsValid(email);
-    correctSubject = subjectIsValid(subject);
+    arr.push(nameIsValid(name));
+    arr.push(emailIsValid(email));
+    arr.push(subjectIsValid(subject));
 
-    if (correctEmail && correctSubject) {
-        console.log(`Your name is ${name} and your's email - ${email}`);
-        console.log(`subject: ${subject} and text - ${text}`);
+    checkAll(arr);
+
+    formPath.reset();
+
+}
+
+// Check all fields for condition
+function checkAll(arr) {
+    if (arr.includes(false)) {
+        alert('Somethin going wrong!')
     } else {
-        alert(`You form isn't correct`);
+        // alert('All good')
     }
-
 }
 
 function emailIsValid(email) {
@@ -29,4 +44,8 @@ function emailIsValid(email) {
 
 function subjectIsValid(subject) {
     return subject.toString().length < 60
+}
+
+function nameIsValid(name) {
+    return name.toString().length >= 1
 }
